fee8dd5956b212f6aac9cbeae28ed1863670aa63
Guilhermeborille <guilherme.borille.if@gmail.com>
2025-04-13 22:41:24 -0300

    teste

--- Main.java
+++ Main.java
@@ -1,9 +1,13 @@
 public class Main {
     public static void main(String[] args){
-        Condutor motora1 = new Condutor("gui", "cnh");
-        Motor motor1 = new Motor("gasosa" , "145 cv");
+        Condutor motorista1 = new Condutor("gui", "cnh");
+        Motor motor1 = new Motor("gasolina" , "145 cv");
         Motor motor2 = new Motor ("eletrico", "135 cv");
-        Carro carro1 = new Carro("fiat", "kwid" , "ab123", motor1, motora1);
+        Carro carro1 = new Carro("fiat", "kwid" , "ab123", motor1, motorista1);
+        Carro carro2 = new Carro("BMW", "BMW 320i M Sport", "CHT76A", motor2);
+
+        System.out.println(carro1.toString());
+        System.out.println(carro2.toString());
 
     }
 }
